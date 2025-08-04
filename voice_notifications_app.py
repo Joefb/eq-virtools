02c47167f0e9e95839b0d9ccea67258734c936e1
Joe Burgess <joeezmail@gmail.com>
2025-08-03 20:39:19 -0700

    Added table editing and auto save with enter key or click out of tabel

--- voice_notifications_app.py
+++ voice_notifications_app.py
@@ -35,9 +35,10 @@ class VoiceNotificationsApp(QWidget):
         self.table = QTableWidget()
         self.table.setColumnCount(2)
         self.table.setHorizontalHeaderLabels(["Log Pattern", "Spoken Message"])
-        self.table.setColumnWidth(0, 280)
-        self.table.setColumnWidth(1, 280)
+        self.table.setColumnWidth(0, 280)  # Log Pattern
+        self.table.setColumnWidth(1, 280)  # Spoken Message
         self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.itemChanged.connect(self.update_trigger)
         self.load_triggers()
         layout.addWidget(self.table)
 
@@ -64,12 +65,18 @@ class VoiceNotificationsApp(QWidget):
         self.setLayout(layout)
 
     def load_triggers(self):
+        self.table.itemChanged.disconnect(self.update_trigger)
         self.table.setRowCount(0)
         for pattern, message in self.triggers.items():
             row = self.table.rowCount()
             self.table.insertRow(row)
-            self.table.setItem(row, 0, QTableWidgetItem(pattern))
-            self.table.setItem(row, 1, QTableWidgetItem(message))
+            pattern_item = QTableWidgetItem(pattern)
+            pattern_item.setFlags(pattern_item.flags() | Qt.ItemFlag.ItemIsEditable)
+            self.table.setItem(row, 0, pattern_item)
+            message_item = QTableWidgetItem(message)
+            message_item.setFlags(message_item.flags() | Qt.ItemFlag.ItemIsEditable)
+            self.table.setItem(row, 1, message_item)
+        self.table.itemChanged.connect(self.update_trigger)
 
     def add_trigger(self):
         pattern = self.pattern_input.text().strip()
@@ -90,6 +97,26 @@ class VoiceNotificationsApp(QWidget):
                 del self.triggers[pattern]
                 self.settings.setValue("voice_triggers", self.triggers)
                 self.load_triggers()
+                self.update_main_app_settings()
+
+    def update_trigger(self, item):
+        row = item.row()
+        pattern_item = self.table.item(row, 0)
+        message_item = self.table.item(row, 1)
+        if pattern_item and message_item:
+            new_pattern = pattern_item.text().strip()
+            new_message = message_item.text().strip()
+            if new_pattern and new_message:
+                # Find old pattern to update
+                old_pattern = None
+                for p, m in list(self.triggers.items()):
+                    if m == self.table.item(row, 1).text() and p != new_pattern:
+                        old_pattern = p
+                        break
+                if old_pattern and old_pattern in self.triggers:
+                    del self.triggers[old_pattern]
+                self.triggers[new_pattern] = new_message
+                self.settings.setValue("voice_triggers", self.triggers)
                 self.update_main_app_settings()
 
     def toggle_notifications(self, state):
