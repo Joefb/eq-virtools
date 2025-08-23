db16a930709f9527afe7d33887db9f1344de8d24
Joe Burgess <joeezmail@gmail.com>
2025-08-23 08:56:28 +0000

    Changed tray-icon.png to new icon.

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ class MainApp:
         self.app.setQuitOnLastWindowClosed(False)
         self.app.setProperty("MainApp", self)
         self.settings = QSettings("EQ-Virtools", "MainApp")
-        self.log_dir = self.settings.value("log_dir", "/home/jfburgess/Games/everquest/Logs/")
+        self.log_dir = self.settings.value("log_dir", "/home/callojoee/Games/everquest/Logs/")
         self.log_path = None
         self.log_file = None
         self.log_position = 0
